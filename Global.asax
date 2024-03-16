5912a474497915f51d2aa6e99848f594520fa9fe
Jigar1992 <jigar.thakkar@tatvasoft.com>
2024-03-16 12:23:20 +0530

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFirstMVCApplicaiton.MvcApplication" Language="C#" %>
---
+++ Models/CustomerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstMVCApplicaiton.Models
+{
+    public class CustomerModel
+    {
+        public int CustomerID   { get; set; }
+        public int CustomerName { get; set; }
+    }
+}
